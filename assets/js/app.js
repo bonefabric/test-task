5930e0d3c0ca29a1f5364ab9d72eb96bacceea33
BoneFabric <iburanguloff@ya.ru>
2022-03-24 21:44:18 +0500

    spa vue

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+import {createApp} from "vue";
+import Application from "./vue/Application";
+
+createApp(Application).mount('#app');
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const {VueLoaderPlugin} = require("vue-loader");
+
+module.exports = {
+    entry: path.resolve(__dirname, './assets/js/app.js'),
+    mode: "development",
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {test: /\.js$/, use: 'babel-loader'},
+            {test: /\.vue$/, loader: 'vue-loader'},
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    output: {
+        path: path.resolve(__dirname, './public/js'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: [".vue", ".js"]
+    }
+}
