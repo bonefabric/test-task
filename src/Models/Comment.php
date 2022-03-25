79138e21176ca84ccaebc3d5cfdd12dd1c3b2739
BoneFabric <iburanguloff@ya.ru>
2022-03-25 13:06:40 +0500

    comments saving

---
+++ src/Models/Comment.php
@@ -0,0 +1,115 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Core\Utility\DB;
+
+class Comment
+{
+    /**
+     * @var string
+     */
+    private string $email = '';
+
+    /**
+     * @var string
+     */
+    private string $title = '';
+
+    /**
+     * @var string
+     */
+    private string $comment = '';
+
+    /**
+     * @var \DateTimeInterface|\DateTime
+     */
+    private \DateTimeInterface $created;
+
+    public function __construct()
+    {
+        $this->created = new \DateTime();
+    }
+
+    /**
+     * @return bool
+     */
+    public function save(): bool
+    {
+        $query = DB::getInstance()->prepare('INSERT INTO comments(email, title, comment, created) VALUES (?, ?, ?, ?)');
+        return $query->execute([$this->getEmail(), $this->getTitle(), $this->getComment(), $this->getCreated()->format('Y-m-d H:i:s')]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return Comment
+     */
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Comment
+     */
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getComment(): string
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @param string $comment
+     * @return Comment
+     */
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime|\DateTimeInterface
+     */
+    public function getCreated(): \DateTime|\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param \DateTime|\DateTimeInterface $created
+     * @return Comment
+     */
+    public function setCreated(\DateTime|\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+        return $this;
+    }
+}
